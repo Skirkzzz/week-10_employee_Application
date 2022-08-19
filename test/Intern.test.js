fb7b25f90799d69369ce31db58f5ea3051ced3d2
Your Namels <selina.kirk82@gmail.com>
2022-08-19 13:45:39 +0100

    Update intern.test and index.js

--- test/Intern.test.js
+++ test/Intern.test.js
@@ -1,66 +1,34 @@
-const answer = require("inquirer");
+const Intern = require("../lib/Intern");
 
-// prompt name, location, bio, LinkedIn URL, and GitHub URL.
-answer.prompt([
-    {
-    type: 'input',
-    message: 'what is your name?',
-    name: 'username',
-    },
-    {
-    type: 'input',
-    message: 'what is your location?',
-    name: 'location',
-    },
-    {
-    type: 'input',
-    message: 'what is your bio?',
-    name: 'bio',
-    },
-    {
-    type: 'input',
-    message: 'what is your LinkedIn URL?',
-    name: 'LinkedIn',
-    },
-    {
-    type: 'input',
-    message: 'what is your GitHub URL?',
-    name: 'GitHub',
-    },
+test("Can create school.", () => {
+  const testSchool = "School Name";
+  const employeeInstance = new Intern(
+    "Lennon",
+    2,
+    "jamesnates@gmail.com",
+    testSchool
+  );
+  expect(employeeInstance.school).toBe(testSchool);
+});
 
-]);
+test("Testing officeNumber will return office number.", () => {
+  const testSchool = "School Name";
+  const employeeInstance = new Intern(
+    "Lennon",
+    2,
+    "Lennonnates@gmail.com",
+    testSchool
+  );
+  expect(employeeInstance.getSchool()).toBe(testSchool);
+});
 
-then((answer) =>
-answer.confirm === answer.input
-
-
-
-const renderHTML = (answer) =>
-{
-    const html= `<!DOCTYPE html>
-    <html lang="en">
-    <head>
-        <meta charset="UTF-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <title>Document</title>
-    </head>
-    <body>
-        <style>
-             body { background-color: rgb(60, 15, 225); }
-            p { color: #fff; font: 18px Arial, sans-serif;}
-            </style>
-    <p>${answer.username}</p>
-    <p>${answer.location}</p>
-    <p>${answer.bio}</p>
-    <p>${answer.LinkedIn}</p>
-    <p>${answer.GitHub}</p>
-
-    </body>
-    </html>`
-
-fs.writeFile("index.html", html, (err) =>
-err ? console.error(`Input entered! Your input is ${answer.input}`)
-:console.log('input is not correct')
-);
-}
+test("Testing role.", () => {
+  const returnValue = "Intern";
+  const employeeInstance = new Intern(
+    "Lennon",
+    2,
+    "Lennonnates@gmail.com",
+    "School Name"
+  );
+  expect(employeeInstance.getRole()).toBe(returnValue);
+});
