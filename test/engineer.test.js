2db740840a9d0711de962584f71cece248e13219
Your Namels <selina.kirk82@gmail.com>
2022-08-19 13:41:56 +0100

    Engineer test.js

--- test/engineer.test.js
+++ test/engineer.test.js
@@ -1,1 +1,34 @@
-const engineerTest = require("./XXXX");
+const Engineer = require("../lib/Engineer");
+
+test("Can create a github.", () => {
+  const testGithub = "BeckySwans";
+  const employeeInstance = new Engineer(
+    "Becky",
+    2,
+    "BeckySwans@gmail.com",
+    testGithub
+  );
+  expect(employeeInstance.github).toBe(testGithub);
+});
+
+test("Testing getGithub will return github.", () => {
+  const testGithub = "BeckySwans";
+  const employeeInstance = new Engineer(
+    "Becky",
+    2,
+    "BeckySwans@gmail.com",
+    testGithub
+  );
+  expect(employeeInstance.getGithub()).toBe(testGithub);
+});
+
+test("Testing role.", () => {
+  const returnValue = "Engineer";
+  const employeeInstance = new Engineer(
+    "Becky",
+    2,
+    "BeckySwans@gmail.com",
+    "BeckySwans"
+  );
+  expect(employeeInstance.getRole()).toBe(returnValue);
+});
